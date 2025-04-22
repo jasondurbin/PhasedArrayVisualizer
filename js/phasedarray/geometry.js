2a58c2533f604e85d5c8eb53464e54303f408d5a
Jason Durbin <jason.durbin@neonphysics.com>
2025-04-21 18:46:10 -0700

    Added spirals.

--- js/phasedarray/geometry.js
+++ js/phasedarray/geometry.js
@@ -4,13 +4,27 @@ export class Geometry{
 	static args = [];
 	static controls = {
 		'geometry': {'title': null},
-		'dx': {'title': "X-Spacing (λ)", 'type': "float", 'min': 0.01},
-		'dy': {'title': "Y-Spacing (λ)", 'type': "float", 'min': 0.01}
+		'dx': {'title': "X-Spacing (λ)", 'type': "float", 'min': 0.0, 'default': 0.5, 'step': 0.1},
+		'dy': {'title': "Y-Spacing (λ)", 'type': "float", 'min': 0.0, 'default': 0.5, 'step': 0.1}
 	};
 	set_xy(x, y){
 		this.length = x.length;
 		this.x = x;
 		this.y = y;
+	}
+	auto_compute_dx_dy(index){
+		let dr = Infinity;
+		if (index === undefined) index = 0;
+		const x1 = this.x[index];
+		const y1 = this.y[index];
+		for (let i = 0; i < this.x.length; i++){
+			if (i == index) continue;
+			const x2 = this.x[i];
+			const y2 = this.y[i];
+			dr = Math.min(dr, Math.sqrt((x1 - x2)**2 + (y1 - y2)**2));
+		}
+		this.dx = dr;
+		this.dy = dr;
 	}
 }
 
@@ -20,7 +34,7 @@ export class RectangularGeometry extends Geometry{
 	static controls = {
 		...Geometry.controls,
 		'x-count': {'title': "X Count", 'type': "int", 'default': 8, 'min': 1},
-		'y-count': {'title': "Y Count", 'type': "int", 'default': 8, 'min': 1}
+		'y-count': {'title': "Y Count", 'type': "int", 'default': 8, 'min': 1},
 	};
 	constructor(dx, dy, xCount, yCount){
 		super();
@@ -91,11 +105,7 @@ export class RectangularOffsetYGeometry extends RectangularGeometry{
 export class Hexagonal extends Geometry{
 	static title = 'Hexagonal';
 	static args = ['dx', 'dy', 'x-count', 'y-count'];
-	static controls = {
-		...Geometry.controls,
-		'x-count': {'title': "X Count", 'type': "int", 'default': 8, 'min': 1},
-		'y-count': {'title': "Y Count", 'type': "int", 'default': 8, 'min': 1}
-	};
+	static controls = RectangularGeometry.controls;
 	constructor(dx, dy, xCount, yCount, axis){
 		super();
 
@@ -166,11 +176,11 @@ export class HexagonalY extends Hexagonal{
 
 export class CircularGeometry extends Geometry{
 	static title = 'Circular';
-	static args = ['dx', 'dy', 'x-count', 'y-count'];
+	static args = ['dx', 'dy', 'min-ring', 'max-ring'];
 	static controls = {
 		...Geometry.controls,
-		'x-count': {'title': 'Start Ring', 'type': "int", 'default': 0, 'min': 0},
-		'y-count': {'title': 'Stop Ring', 'type': "int", 'default': 8, 'min': 1}
+		'min-ring': {'title': 'Start Ring', 'type': "int", 'default': 0, 'min': 0},
+		'max-ring': {'title': 'Stop Ring', 'type': "int", 'default': 8, 'min': 1},
 	};
 	constructor(dx, dy, minRing, maxRing){
 		super();
@@ -215,12 +225,8 @@ export class CircularGeometry extends Geometry{
 
 export class SunflowerGeometry extends Geometry{
 	static title = 'Sunflower';
-	static args = ['dx', 'dy', 'x-count', 'y-count'];
-	static controls = {
-		...Geometry.controls,
-		'x-count': {'title': 'Start Ring', 'type': "int", 'default': 0, 'min': 0},
-		'y-count': {'title': 'Stop Ring', 'type': "int", 'default': 8, 'min': 1}
-	};
+	static args = ['dx', 'dy', 'min-ring', 'max-ring'];
+	static controls = CircularGeometry.controls;
 	constructor(dx, dy, minRing, maxRing){
 		super();
 
@@ -271,6 +277,179 @@ export class SunflowerGeometry extends Geometry{
 		this.set_xy(Float32Array.from(x), Float32Array.from(y));
 	}
 }
+
+export class ArchimedianSpiral extends Geometry{
+	static title = 'Archimedian Spiral';
+	static args = ['r0', 'r-max', 'spirals', 'count-per-spiral', 'total-rotation'];
+	static controls = {
+		'geometry': {'title': null},
+		'r0': {'title': "R-Start (λ)", 'type': "float", 'min': 0.0, 'default': 0.5},
+		'r-max': {'title': "R-Max (λ)", 'type': "float", 'min': 0.0, 'default': 2.5},
+		'spirals': {'title': 'Spirals', 'type': "int", 'default': 1, 'min': 1},
+		'count-per-spiral': {'title': 'Elements per Spiral', 'type': "int", 'default': 31, 'min': 1},
+		'total-rotation': {'title': 'Total Rotations (deg)', 'type': "float", 'default': 990, 'min': 0},
+	};
+	constructor(r0, rmax, spirals, count, rotations){
+		super();
+
+		if (r0 <= 0) r0 = 0.0
+		if (rmax <= 0) rmax = 0.5
+
+		this.r0 = Number(r0);
+		this.rmax = Number(rmax);
+		if (spirals <= 0) spirals = 1;
+		this.spirals = Number(spirals);
+
+		this.rotations = Math.max(0, Number(rotations))*Math.PI/180;
+		this.count = Number(count);
+	}
+	build(){
+		let x = []
+		let y = []
+		let sc = (this.rmax - this.r0)/this.rotations;
+		let rc = this.rotations/(this.count - 1);
+
+		let index = 0;
+		if (this.spirals > 1){
+			x.push(0.0);
+			y.push(0.0);
+			index = 1;
+		}
+
+		for (let j = 0; j < this.spirals; j++){
+			const io = 2*Math.PI/this.spirals*j;
+			for (let i = 0; i < this.count; i++){
+				const phi = i*rc;
+				const r = this.r0 + sc*phi;
+				x.push(r*Math.cos(phi + io));
+				y.push(r*Math.sin(phi + io));
+			}
+		}
+		this.set_xy(Float32Array.from(x), Float32Array.from(y));
+		this.auto_compute_dx_dy(index);
+	}
+}
+
+export class DoughertyLogSpiral extends Geometry{
+	static title = 'Dougherty Log-Spiral';
+	static args = ['r0', 'r-max', 'spirals', 'count-per-spiral', 'offset-rotation'];
+	static controls = {
+		'geometry': {'title': null},
+		'r0': {'title': "R-Start (λ)", 'type': "float", 'min': 0.01, 'default': 0.5},
+		'r-max': {'title': "R-Max (λ)", 'type': "float", 'min': 0.0, 'default': 2.5},
+		'spirals': {'title': 'Spirals', 'type': "int", 'default': 1, 'min': 1},
+		'count-per-spiral': {'title': 'Elements per Spiral', 'type': "int", 'default': 31, 'min': 1},
+		'offset-rotation': {'title': 'Angle (deg)', 'type': "float", 'default': 84, 'min': 0},
+	};
+	constructor(r0, rmax, spirals, count, rotations){
+		super();
+
+		if (r0 <= 0) r0 = 0.5
+		if (rmax <= 0) rmax = 2.0
+
+		this.r0 = Number(r0);
+		this.rmax = Number(rmax);
+
+		if (spirals <= 0) spirals = 1;
+		this.spirals = Number(spirals);
+
+		this.rotations = Math.max(0, Number(rotations))*Math.PI/180;
+		this.count = Number(count);
+	}
+	build(){
+		const cov = 1/Math.tan(this.rotations);
+		const sc = 1/(this.count-1)*(this.rmax/this.r0 - 1);
+
+		let x = []
+		let y = []
+		let index = 0;
+		if (this.spirals > 1){
+			x.push(0.0);
+			y.push(0.0);
+			index = 1;
+		}
+
+		let phi, r;
+		for (let j = 0; j < this.spirals; j++){
+			const io = 2*Math.PI/this.spirals*j;
+			for (let i = 0; i < this.count; i++){
+				if (this.rotations == 0){
+					phi = 0;
+					r = this.r0 + i*(this.rmax-this.r0)/(this.count-1);
+				}
+				else{
+					phi = 1/cov*Math.log(1 + i*sc);
+					r = this.r0*Math.exp(cov*phi);
+				}
+
+				console.log(r, phi, Math.exp(cov*phi), phi, cov);
+				x.push(r*Math.cos(phi + io));
+				y.push(r*Math.sin(phi + io));
+			}
+		}
+		this.set_xy(Float32Array.from(x), Float32Array.from(y));
+		this.auto_compute_dx_dy(index);
+	}
+}
+
+export class ArcondoulisSpiral extends Geometry{
+	static title = 'Arcondoulis Spiral';
+	static args = ['r0', 'r-max', 'spirals', 'count-per-spiral', 'total-rotation', 'eta-x', 'eta-y'];
+	static controls = {
+		'geometry': {'title': null},
+		'r0': {'title': "R-Start (λ)", 'type': "float", 'min': 0.01, 'default': 0.5, 'step': 0.1},
+		'r-max': {'title': "R-Max (λ)", 'type': "float", 'min': 0.0, 'default': 2.5, 'step': 0.1},
+		'spirals': {'title': 'Spirals', 'type': "int", 'default': 1, 'min': 1},
+		'count-per-spiral': {'title': 'Elements per Spiral', 'type': "int", 'default': 31, 'min': 1},
+		'total-rotation': {'title': 'Total Rotations (deg)', 'type': "float", 'default': 990, 'min': 0},
+		'eta-x': {'title': 'Squashing-X', 'type': "float", 'default': 0.9, 'min': 0, 'step': 0.1},
+		'eta-y': {'title': 'Squashing-X', 'type': "float", 'default': 0.9, 'min': 0, 'step': 0.1},
+	};
+	constructor(r0, rmax, spirals, count, rotations, ex, ey){
+		super();
+
+		if (r0 <= 0) r0 = 0.5
+		if (rmax <= 0) rmax = 2.0
+		if (ex <= 0) ex = 0.9
+		if (ey <= 0) ey = 0.9
+		if (spirals <= 0) spirals = 1;
+		this.spirals = Number(spirals);
+
+		this.r0 = Number(r0);
+		this.rmax = Number(rmax);
+		this.ex = Number(ex);
+		this.ey = Number(ey);
+
+		this.rotations = Math.max(0, Number(rotations))*Math.PI/180;
+		this.count = Number(count);
+	}
+	build(){
+		const a = this.r0*(this.count/(this.ex*this.count + 1));
+		const b = 1/this.rotations*Math.log(this.rmax/(a*Math.sqrt((1 + this.ex)**2*Math.cos(this.rotations)**2+(1 + this.ey)**2*Math.sin(this.rotations)**2)));
+
+		let x = []
+		let y = []
+		const sc = this.rotations/(this.count - 1);
+		let index = 0;
+		if (this.spirals > 1){
+			x.push(0.0);
+			y.push(0.0);
+			index = 1;
+		}
+		for (let j = 0; j < this.spirals; j++){
+			const io = 2*Math.PI/this.spirals*j;
+			for (let i = 0; i < this.count; i++){
+				const phi = i*sc;
+				const m = a*Math.exp(b*phi);
+				x.push((i + 1 + this.ex*this.count)/this.count*Math.cos(phi + io)*m);
+				y.push((i + 1 + this.ey*this.count)/this.count*Math.sin(phi + io)*m);
+			}
+		}
+		this.set_xy(Float32Array.from(x), Float32Array.from(y));
+		this.auto_compute_dx_dy(index);
+	}
+}
+
 export const Geometries = [
 	RectangularGeometry,
 	RectangularOffsetXGeometry,
@@ -279,4 +458,7 @@ export const Geometries = [
 	HexagonalY,
 	CircularGeometry,
 	SunflowerGeometry,
+	ArchimedianSpiral,
+	DoughertyLogSpiral,
+	ArcondoulisSpiral,
 ]
